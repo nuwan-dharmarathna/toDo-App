e53cecc87c59ab3047f1fd91acf0982e1e08de07
meow-4O4 <dharmarathna1220@gmail.com>
2023-12-01 22:11:38 +0530

    build error handling part

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+// error handlers
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
+
 // import routes
 const userRoutes = require('./routes/userRoutes');
 const toDoRoutes = require('./routes/toDoRoutes');
@@ -12,5 +16,11 @@ app.use(express.json());
 // implement routes
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/todo', toDoRoutes);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Cant find ${req.originalUrl} on this server!`));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- backend/controllers/toDoControllers.js
+++ backend/controllers/toDoControllers.js
@@ -1,36 +1,23 @@
 const ToDo = require('./../models/toDoModel');
+const catchAsync = require('./../utils/catchAsync');
 
-exports.getAllToDos = async (req, res) => {
-  try {
-    const toDos = await ToDo.find();
-    res.status(200).json({
-      status: 'Success',
-      results: toDos.length,
-      data: {
-        toDos: toDos,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'Failed',
-      message: err,
-    });
-  }
-};
+exports.getAllToDos = catchAsync(async (req, res) => {
+  const toDos = await ToDo.find();
+  res.status(200).json({
+    status: 'Success',
+    results: toDos.length,
+    data: {
+      toDos: toDos,
+    },
+  });
+});
 
-exports.createToDo = async (req, res) => {
-  try {
-    const newToDo = await ToDo.create(req.body);
-    res.status(200).json({
-      status: 'Success',
-      data: {
-        toDos: newToDo,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'Failed',
-      message: err,
-    });
-  }
-};
+exports.createToDo = catchAsync(async (req, res, next) => {
+  const newToDo = await ToDo.create(req.body);
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      toDos: newToDo,
+    },
+  });
+});
